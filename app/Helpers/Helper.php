160c666d8664106ad4e63a04647c3837940a3caa
Vu My <tranhoangvumy22@gmail.com>
2024-03-18 22:35:31 +0700

    Add Style for product card

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -13,3 +13,47 @@ function isTableEmpty($model){
     return $model->isEmpty();
 }
  
+// Check sale 
+
+function checkSale($product)
+{
+    $currentDate = Date("Y-m-d");
+    if (
+        $product->offer_price > 0 && $currentDate >= $product->offer_start_price
+        && $currentDate <=  $product->offer_end_price
+    ) return true;
+    else return false;
+}
+
+// Calculate Sale
+
+function calculateSalePercent($product)
+{
+    $discountPrice = $product->price - $product->offer_price;
+    $percent =  ($discountPrice / $product->price) * 100;
+    return number_format($percent,0);
+}
+
+// Get Product type
+function getProductType($product)
+{
+    $type = "";
+    switch ($product->product_type) {
+        case 'top':
+            $type = "TOP";
+            break;
+        case 'best':
+            $type = "BEST";
+            break;
+        case 'featured':
+            $type = "FEATURED";
+            break;
+        case 'new_arrival':
+            $type = "NEW";
+            break;
+        default:
+            $type = null;
+            break;
+    }
+    return $type;
+}
--- resources/views/frontend/home/sections/top-product.blade.php
+++ resources/views/frontend/home/sections/top-product.blade.php
@@ -4,15 +4,26 @@
     <ul class="grid  grid-cols-6 py-5 gap-3 cursor-pointer ">
         @foreach ($topProducts as $t)
             <li
-                class="hover:shadow-xl hover:-translate-y-1 transition-all hover:border-cyan-600 flex flex-col justify-between border-2 leading-8  border-slate-100">
+                class="relative hover:shadow-xl hover:-translate-y-1 transition-all hover:border-cyan-600 flex flex-col justify-between border-2 leading-8  border-slate-100">
                 <img class="min-h-[180px] w-full" src="{{ asset($t->thumb_image) }}" />
-                <div class="p-2">
+                <div class="absolute w-full flex justify-between">
+                    <span class="bg-sky-700 rounded-sm text-white text-sm  py-1 px-2">
+                        {{ getProductType($t) }}
+                    </span>
+                    @if (checkSale($t))
+                        <span class="bg-sky-700 rounded-sm text-white text-sm py-1 px-2">
+                            {{ calculateSalePercent($t) . '%' }}
+                        </span>
+                    @endif
+                </div>
+                <div class=" p-2">
                     <h1>{{ $t->name }}</h1>
                     <p class="flex justify-between items-center">
                         <span class="text-orange-500 font-bold">${{ $t->price }}</span>
                         <span class="text-sm ">30 Sold</span>
                     </p>
                 </div>
+
             </li>
         @endforeach
     </ul>
