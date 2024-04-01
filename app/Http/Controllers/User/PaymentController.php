5c41b3a5af9ddf69237f77bd91e2ffeb7259a184
Vu My <tranhoangvumy22@gmail.com>
2024-04-01 21:34:21 +0700

    Store Order Producr

--- app/Http/Controllers/User/PaymentController.php
+++ app/Http/Controllers/User/PaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\OrderProduct;
 use Carbon\Carbon;
 use Cart;
 use Illuminate\Http\Request;
@@ -22,7 +23,7 @@ class PaymentController extends Controller
         $current = Carbon::now(); 
         $invoice_id = Auth::user()->id;
         $invoice_id .= $current->isoFormat('YYMMDDkkmm');
-        Order::create([
+        $order =  Order::create([
             'invoice_id' =>  $invoice_id, 
             'user_id' => Auth::user()->id, 
             'sub_total' => Cart::getSubTotal(),
@@ -33,7 +34,25 @@ class PaymentController extends Controller
             "user_address_id"=> $request->order_address,
             "coupon" =>  $coupon ,
             "order_status" => "pending", 
-        ]);
+        ]); 
+        foreach(Cart::getContent() as $item){  
+            $variants = array();  
+            $index = 0;
+            foreach($item->attributes as $key => $attr){
+                if($index > 3)  $variants[$key] = $attr;
+                $index++;
+            }
+            OrderProduct::create([
+                'order_id' =>  $order->id, 
+                'product_id' => $item->attributes->product_id, 
+                'vendor_id' => $item->attributes->vendor_id,
+                'product_name' => $item->name, 
+                "variants" => json_encode($variants), 
+                "variant_total"=> $index-4, 
+                "unit_price" => $item->price, 
+                "qty" => $item->quantity, 
+            ]);
+        };
         return view("frontend.pages.payment-success");
     }
 }
